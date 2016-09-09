18088edb8eba31eeeadaa4576e286b8795f1aaeb
José Carlos Martins <naoedojose.nave@gmail.com>
2016-09-09 14:43:43 -0300

    Start of the Project
    Start:
    -Node;
    -Grid ;

---
+++ Assets/Grid.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	Node[,] grid;
+
+	float nodeDiameter;
+	[Range(4,32)]
+	int gridSizeX;
+	[Range(4,32)]
+	int gridSizeY;
+
+
+	void Start(){
+		nodeDiameter = nodeRadius * 2;
+		CreateGrid ();
+	}
+
+	void CreateGrid(){
+		grid = new Node[ gridSizeX, gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+	
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireCube (transform.position, new Vector3 (gridWorldSize.x,1,gridWorldSize.y));
+	
+	}
+
+
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	public bool walkable;
+	public Vector3 worldPosition;
+
+	public Node( bool _walkable , Vector3 _worldPosition){
+		_walkable = walkable;
+		_worldPosition = worldPosition;
+	}
+}
