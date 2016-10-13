8d2f6b9630a4bf5d53d891213cdaf144de8fd42d
NaoedoJose <naoedojose.nave@gmail.com>
2016-10-13 14:26:46 -0300

    Pathfind ready-ish
    Working on other colors in Gizmos
    Need some optimization, but i don't want to do it :)

--- Assets/_Scripts/Grid.cs
+++ Assets/_Scripts/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Grid : MonoBehaviour {
 
@@ -14,6 +15,7 @@ public class Grid : MonoBehaviour {
 
 	float nodeDiameter;
 	int gridSizeX, gridSizeY;
+	Vector3 publicWorldBottomLeft;
 
 	void Start() {
 		nodeDiameter = nodeRadius*2;
@@ -23,19 +25,33 @@ public class Grid : MonoBehaviour {
 		CreateGrid();
 	}
 
+	public List<Node> GetNeighbours(Node node) {
+		List<Node> neighbours = new List<Node>();
+
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				if (x == 0 && y == 0)
+					continue;
+
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+					neighbours.Add(grid[checkX,checkY]);
+				}
+			}
+		}
+
+		return neighbours;
+	}
+
+
 	void CreateGrid() {
 		grid = new Node[gridSizeX,gridSizeY];
 		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
-
-		for (int x = 0; x < gridSizeX; x ++) {
-			for (int y = 0; y < gridSizeY; y ++) {
-				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
-				grid[x,y] = new Node(walkable,worldPoint);
-			}
-		}
+		publicWorldBottomLeft = worldBottomLeft;
 	}
-
+		
 	public Node NodeFromWorldPoint(Vector3 worldPosition) {
 		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
 		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
@@ -46,27 +62,28 @@ public class Grid : MonoBehaviour {
 		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
 		return grid[x,y];
 	}
-
+	public List<Node> path;
 	void OnDrawGizmos() {
 		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
-
 
 		if (grid != null) {
 			foreach (Node n in grid) {
 				Gizmos.color = (n.walkable)?Color.white:Color.red;
+				if (path != null)
+					if (path.Contains(n))
+						Gizmos.color = Color.yellow;
 				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
+			}
+		}
+	}
+	void Update(){
+		for (int x = 0; x < gridSizeX; x ++) {
+			for (int y = 0; y < gridSizeY; y ++) {
+				Vector3 worldPoint = publicWorldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+				grid[x,y] = new Node(walkable,worldPoint,x,y);
 			}
 		}
 	}
 }
 
-public class Node {
-
-	public bool walkable;
-	public Vector3 worldPosition;
-
-	public Node(bool _walkable, Vector3 _worldPos) {
-		walkable = _walkable;
-		worldPosition = _worldPos;
-	}
-}
---
+++ Assets/_Scripts/Node.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node {
+
+	public string state;
+	public Vector3 worldPosition;
+
+	public int gCost;
+	public int hCost;
+
+	public int gridX;
+	public int gridY;
+	public bool walkable;
+
+	public Node parent;
+
+	public Node(string _state, Vector3 _worldPos , int _gridX, int _gridY) {
+
+		state = _state;
+		worldPosition = _worldPos;
+		gridX = _gridX;
+		gridY = _gridY;
+		switch (state) {
+		case "walkable":
+			walkable = true;
+			return;
+		}
+			
+	}
+
+	public int fCost{
+		get{ 
+			return gCost + hCost;
+		}
+	}
+}
